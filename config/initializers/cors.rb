0e94bbd478db7be7646a6f479e7286342b8d1276
Ryan Ghaida <ryaboug@gmail.com>
2019-10-21 14:51:51 -0400

    app test

---
+++ config/initializers/cors.rb
@@ -0,0 +1,18 @@
+#gives you ability to whitlist certain domains.
+#give rules for how your going to be able to communicate.
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+  allow do 
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true 
+  end 
+  #url for app below
+  allow do 
+    origins "http://JournalRailsReactApp.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true 
+  end 
+end 
+
+
+
+
