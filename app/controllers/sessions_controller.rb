f738e53b9a83e8b97dabefb9355014292a652af8
Ryan Ghaida <ryaboug@gmail.com>
2019-10-23 13:03:38 -0400

    login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController 
 include CurrentUserConcern
   def create 
-    byebug
     user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
 
     if user 
       session[:user_id] = user.id
       render json: {
-        status: created,
+        status: "created",
         logged_in: true,
         user: user 
       }
