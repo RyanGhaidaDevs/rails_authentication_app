b4d6059bb684092454ec889754641b0ae0380026
Ryan Ghaida <ryaboug@gmail.com>
2020-01-24 15:47:04 -0500

    changing cors in config file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end 
   #url for app below
   allow do 
-    origins "http://JournalRailsReactApp.herokuapp.com"
+    origins "https://bugloggerapp.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true 
   end 
 end 
