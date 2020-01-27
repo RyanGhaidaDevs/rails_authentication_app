46592113a50d56747640c9d592a94ff3ea65f793
Ryan Ghaida <ryaboug@gmail.com>
2020-01-27 15:30:28 -0500

    changed session store to development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env === "production"
+if Rails.env === "development"
   Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://bugloggerapp.herokuapp.com/"
 else 
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
